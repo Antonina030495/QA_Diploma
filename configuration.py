de823ab039bda03826021a323380e46d2c7f25db
Antonina95 <antoninazhabassova95@gmail.com>
2024-04-27 14:58:40 +0300

    commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Жабасова Антонина, 15-я когорта — Дипломный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://30637739-7612-4388-8ba5-f61cb2146bfb.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_TRACK_ORDER = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,18 @@
+# Жабасова Антонина, 15-я когорта — Дипломный проект. Инженер по тестированию плюс
+headers = {
+    "Content-Type": "application/json"
+}
+order_body = {
+    "firstName": "Naruto",
+    "lastName": "Uchiha",
+    "address": "Konoha, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 355 35 35",
+    "rentTime": 5,
+    "deliveryDate": "2020-06-06",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
+
